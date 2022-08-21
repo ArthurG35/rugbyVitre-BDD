470e35e4248e3a84ddc97d36c236c14a6cb825c1
Arthur <arthur.gougeon17@hotmail.com>
2022-08-21 18:47:27 +0200

    BASE

---
+++ src/main/java/com/arthur/fr/rugbyvitre/RugbyvitreApplication.java
@@ -0,0 +1,13 @@
+package com.arthur.fr.rugbyvitre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RugbyvitreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RugbyvitreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthur/fr/rugbyvitre/RugbyvitreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.fr.rugbyvitre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RugbyvitreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
