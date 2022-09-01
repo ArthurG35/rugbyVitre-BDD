55593022d91d524998e0563b223cffb587dba4b1
Benjamin <benjamin.theron.bt@gmail.com>
2022-09-01 08:31:11 +0100

    Added @Mapping

--- src/main/java/com/arthur/fr/rugbyvitre/mapper/JoueurMapper.java
+++ src/main/java/com/arthur/fr/rugbyvitre/mapper/JoueurMapper.java
@@ -3,15 +3,16 @@ package com.arthur.fr.rugbyvitre.mapper;
 import com.arthur.fr.rugbyvitre.api.dto.JoueurDto;
 import com.arthur.fr.rugbyvitre.model.Joueur;
 import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 
 @Component
 @Mapper(componentModel = "spring")
 public interface JoueurMapper {
 
+    @Mapping(target = "id", source = "id")
     JoueurDto mapToDto(Joueur joueur);
-
+    @Mapping(target = "id", source = "id")
     Joueur mapToModel(JoueurDto joueurDto);
 
 
